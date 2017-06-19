46188cab916082ac07e78111069b38a8670b46bb
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-06-19 04:41:02 -0700

    .gitignore added and SignIn View is being built

---
+++ Virtual-Tourist/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  Virtual-Tourist
+//
+//  Created by Zulwiyoza Putra on 6/19/17.
+//  Copyright © 2017 Zulwiyoza Putra. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signInButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
+        signUpButton.addTarget(self, action: #selector(signUp), for: .touchUpInside)
+
+    }
+    
+    func signIn() -> Bool {
+        print("Sign In Button Tapped")
+        return false
+    }
+    
+    func signUp() -> Bool {
+        print("Sign In Button Tapped")
+        return false
+    }
+    
+}
